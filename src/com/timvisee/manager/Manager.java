5ed3c2bbf94d988dbb531aa5605a442b50101ed6
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2012-10-27 20:45:13 +0200

    Build 4
    Added permission system testing commands.

--- src/com/timvisee/manager/Manager.java
+++ src/com/timvisee/manager/Manager.java
@@ -2,8 +2,10 @@ package com.timvisee.manager;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.timvisee.manager.economymanager.EconomyManager;
@@ -65,6 +67,30 @@ public class Manager extends JavaPlugin {
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if(sender instanceof Player){
+			if(cmd.getName().equalsIgnoreCase("manager")){
+				if(args.length>0){
+					if(args[0].equalsIgnoreCase("node")){
+						if(args.length>1){
+							if(this.pm.hasPermission((Player) sender, args[1]))
+								sender.sendMessage("Passed");
+							else
+								sender.sendMessage("Failed");
+							return true;
+						} else {
+							sender.sendMessage("No permission node provided.");
+							return false;
+						}
+					} else if(args[0].equalsIgnoreCase("groups")){
+						for(Object o: this.pm.getGroups((Player) sender)){
+							sender.sendMessage(o.toString());
+						}
+						return true;
+					}
+					
+				}
+			} else return false;
+		}
 		return false;
 	}
 }
