2abeb56b16c05f1a8741de5f44ad8e475ba820e2
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2012-12-24 17:54:10 +0100

    Removed duplicated enum

--- src/com/timvisee/manager/economymanager/EconomyManager.java
+++ src/com/timvisee/manager/economymanager/EconomyManager.java
@@ -852,21 +852,4 @@ public class EconomyManager {
 			}
         }
 	}
-
-	public enum EconomySystemType {
-		NONE("None"),
-		SIMPLE_ECONOMY("Simple Economy"),
-		BOSECONOMY("BOSEconomy"),
-		VAULT("Vault");
-		
-		public String name;
-		
-		EconomySystemType(String name) {
-			this.name = name;
-		}
-		
-		public String getName() {
-			return this.name;
-		}
-	}
 }
