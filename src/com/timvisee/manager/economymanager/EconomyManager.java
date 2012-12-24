b0d7329f67915c176e3fa1a06ee5650ac6375644
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2012-12-24 13:33:00 +0100

    Build 14.4
    Moved EconomySystemType and PermissionsSystemType enums into the
    EconomyManager and PermissionsManager files respectively.

--- src/com/timvisee/manager/economymanager/EconomyManager.java
+++ src/com/timvisee/manager/economymanager/EconomyManager.java
@@ -32,6 +32,29 @@ import java.lang.reflect.Method;                // AEco
 import net.milkbowl.vault.economy.Economy;
 
 public class EconomyManager {
+	public enum EconomySystemType {
+		NONE("None"),
+		SIMPLE_ECONOMY("Simple Economy"),
+		BOSECONOMY("BOSEconomy"),
+		ESSENTIALS("Essentials"),
+		MINECONOMY("MineConomy"),
+		EWALLET("eWallet"),
+		ECONXP("EconXP"),
+		CURRENCYCORE("CurrencyCore"),
+		CRAFTCONOMY("CraftConomy"),
+		AECO("AEco"),
+		VAULT("Vault");
+		
+		public String name;
+		
+		EconomySystemType(String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+	}
 	
 	private EconomySystemType economyType = EconomySystemType.NONE;
 	private Server s;
--- src/com/timvisee/manager/permissionsmanager/PermissionsManager.java
+++ src/com/timvisee/manager/permissionsmanager/PermissionsManager.java
@@ -28,6 +28,26 @@ import de.bananaco.bpermissions.api.ApiLayer;
 import de.bananaco.bpermissions.api.util.CalculableType;
 
 public class PermissionsManager {
+	public enum PermissionsSystemType {
+		NONE("None"),
+		PERMISSIONS_EX("Permissions Ex"),
+		PERMISSIONS_BUKKIT("Permissions Bukkit"),
+		B_PERMISSIONS("bPermissions"),
+		ESSENTIALS_GROUP_MANAGER("Essentials Group Manager"),
+		Z_PERMISSIONS("zPermissions"),
+		VAULT("Vault"),
+		PERMISSIONS("Permissions");
+		
+		public String name;
+		
+		PermissionsSystemType(String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+	}
 	
 	private PermissionsSystemType permsType = PermissionsSystemType.NONE;
 	private Server s;
